2ceb5a3adfbd00589ece740a137f1b31a3df9e36
igoryan <ihor.karabut01@gmail.com>
2025-03-25 12:47:13 +0000

    first release of my random number generator

---
+++ README.md
@@ -0,0 +1,2 @@
+PLEASE READ THE INFO.TXT FILE
+it has all the required information you need to use the script properly
---
+++ script.py
@@ -0,0 +1,6 @@
+import random
+
+data = input("please enter a max number: ")
+number = random.randint(0,int(data))
+print("here`s your random number ")
+print(number)
