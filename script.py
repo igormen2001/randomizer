dc13ee1aa6829bb2ca36fc7496f8a72156659367
igoryan <ihor.karabut01@gmail.com>
2025-03-26 08:48:02 +0000

    super_randomizer got nice user friendly interface and a default value

--- script.py
+++ script.py
@@ -1,6 +1,8 @@
 import random
-
+print("welcome to super randomizer\n======================================================")
 data = input("please enter a max number: ")
+if data == "":
+	data = 10
 number = random.randint(0,int(data))
 print("here`s your random number ")
 print(number)
