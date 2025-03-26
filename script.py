e17adbac1d2f1a3896ed3dd71cec9aa912b260a0
igoryan <ihor.karabut01@gmail.com>
2025-03-26 09:00:56 +0000

    i just decided to ask for not enetering a zero value

--- script.py
+++ script.py
@@ -1,6 +1,6 @@
 import random
 
-data = input("please enter a max number: ")
+data = input("please enter a max number(no empty value please): ")
 number = random.randint(0,int(data))
 print("here`s your random number ")
 print(number)
