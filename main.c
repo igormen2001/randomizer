e1b15dd3b145fb701962adaec9f53a897872c57f
igoryan <ihor.karabut01@gmail.com>
2025-03-27 14:08:17 +0000

    c nailed it

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+	int a,b,c;
+	printf("print first number: ");
+	scanf("%d",&a);
+	printf("print second number: ");
+	scanf("%d",&b);
+	c = a+b;
+	printf("%d and %d will be summed into %d\n",a,b,c);
+	return 0;
+}
